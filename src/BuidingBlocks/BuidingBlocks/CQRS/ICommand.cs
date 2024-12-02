3646e2609711ac718b5a242054bc8b403e8fd746
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-12-03 01:40:39 +0200

    add BuidingBlocks

---
+++ src/BuidingBlocks/BuidingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System.Net;
+
+namespace BuidingBlocks.CQRS;
+
+public interface ICommand : ICommand <Unit>
+{
+
+}
+
+public interface ICommand<out TResponse>  : IRequest<TResponse> 
+{   
+
+}
---
+++ src/BuidingBlocks/BuidingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+
+namespace BuidingBlocks.CQRS;
+
+public interface IQueryHandler<in TQuery,TReponse>
+                :IRequestHandler<TQuery, TReponse>
+    where TQuery : IOuery<TReponse>
+    where  TReponse :notnull
+{
+
+}
+
