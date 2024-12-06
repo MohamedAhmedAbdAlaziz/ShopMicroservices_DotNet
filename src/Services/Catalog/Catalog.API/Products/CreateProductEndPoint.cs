378cf76a8f7a47fc0b4f9cc75cbdb5f32f1b9675
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-12-06 02:57:15 +0200

    add Catigory Entpoint

--- src/Services/Catalog/Catalog.API/Products/CreateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProductEndPoint.cs
@@ -1,6 +1,31 @@
-﻿namespace Catalog.API.Products
+﻿using BuidingBlocks.CQRS;
+using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products
 {
-    public class CreateProductEndPoint
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.Map("/products",
+               async (CreateProductRequest request, ISender sender) =>
+               {
+                   var command = request.Adapt<CreateProductCommand>();
+                   var result = await sender.Send(command);
+                   var response = result.Adapt<CreateProductResponse>();
+                   return Results.Created($"/products/{response.Id}", response);
+               }).WithName("CreateProduct")
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Product")
+                .WithDescription("Create Product");
+
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/Products/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProductHandler.cs
@@ -1,16 +1,30 @@
-﻿using MediatR;
+﻿using BuidingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+using System.Windows.Input;
 
 namespace Catalog.API.Products
 {
 
-    public record CreateProductCommand(string Name , List<string> Category, string Description, string ImageFile , decimal Price) :IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) :ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductHandler  : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+
+            return new CreateProductResult(Guid.NewGuid());
+           
         }
     }
 }
