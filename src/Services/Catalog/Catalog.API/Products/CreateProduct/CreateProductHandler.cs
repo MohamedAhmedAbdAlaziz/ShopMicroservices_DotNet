391c8065a38fa5f66d28b76600d86c3269bdaba4
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-12-08 20:44:57 +0200

    add Validator

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,6 @@
 ﻿using BuidingBlocks.CQRS;
 using Catalog.API.Models;
+using FluentValidation;
 using Marten;
 using MediatR;
 using System.Windows.Input;
@@ -10,26 +11,44 @@ namespace Catalog.API.Products.CreateProduct
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler(IDocumentSession documentSession) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        public CreateProductCommandValidator()
         {
-            var product = new Product
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Price is required");
+        }
+
+        public class CreateProductHandler(IDocumentSession documentSession, IValidator<CreateProductCommand> validator) : ICommandHandler<CreateProductCommand, CreateProductResult>
+        {
+            public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
             {
-                Name = command.Name,
-                Category = command.Category,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price,
-            };
+                var result = await validator.ValidateAsync(command, cancellationToken);
+                var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+                if (errors.Any())
+                {
+                    throw new ValidationException(errors.FirstOrDefault());
+                }
+                var product = new Product
+                {
+                    Name = command.Name,
+                    Category = command.Category,
+                    Description = command.Description,
+                    ImageFile = command.ImageFile,
+                    Price = command.Price,
+                };
 
 
-            documentSession.Store(product);
-            await documentSession.SaveChangesAsync(cancellationToken);
-            return new CreateProductResult(product.Id);
+                documentSession.Store(product);
+                await documentSession.SaveChangesAsync(cancellationToken);
+                return new CreateProductResult(product.Id);
 
 
 
+            }
         }
     }
 }
