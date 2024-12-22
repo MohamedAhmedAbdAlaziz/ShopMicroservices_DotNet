5198258a645492ce7361c7e700f73a4d368954c2
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-12-22 23:40:10 +0200

    Add Ordering Layers

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IEntity
+    {
+        public DateTime? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
