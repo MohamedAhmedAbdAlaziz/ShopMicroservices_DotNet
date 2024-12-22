94217fbe283a6789d7bcc88b82096272dbe7b4b7
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-12-23 01:52:37 +0200

    Add Iaggregation

--- src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Ordering.Domain.Abstractions
 {
+    public interface IEntity<T>: IEntity
+    {
+        public  T Id { get; set; }  
+    }
     public interface IEntity
     {
         public DateTime? CreatedAt { get; set; }
