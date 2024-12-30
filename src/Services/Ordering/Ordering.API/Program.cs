cdcdd40e25e2fcea46687e431d08473bc8a72cfa
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2024-12-30 23:52:18 +0200

    apdate ordering

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,6 @@
+using Ordering.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddInfrastructureServices(builder.Configuration);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
