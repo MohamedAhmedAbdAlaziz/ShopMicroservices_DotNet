1c16e11597e2ff03da47ebf86e6fb1bdda81d8a2
Mohamed Sallam <mohamed.ahmed799999@gmail.com>
2025-01-11 11:54:54 +0200

    updating in ordering db

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,8 +1,15 @@
+using Ordering.API;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructureServices(builder.Configuration);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
